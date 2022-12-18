acf2f5c6e0aac609dff83416be0732977e05a1b7
cyanobot <119729214+cyanobot@users.noreply.github.com>
2022-12-18 10:24:20 +0000

    Add project files.

---
+++ Source/FlushBiotuning/FlushBiotuning_Settings.cs
@@ -0,0 +1,60 @@
+﻿using RimWorld;
+using Verse;
+using Verse.AI;
+using HarmonyLib;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlushBiotuning
+{
+    public class FlushBiotuning_Settings : ModSettings
+    {
+        public static float flushCycleDuration = 7f;
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+
+            Scribe_Values.Look(ref flushCycleDuration, "flushCycleDuration", flushCycleDuration, true);
+        }
+
+        public static void DoSettingsWindowContents(Rect rect)
+        {
+            Listing_Standard l = new Listing_Standard(GameFont.Small)
+            {
+                ColumnWidth = rect.width
+            };
+
+            l.Begin(rect);
+
+            l.Label("Flush Cycle Duration (days): " + flushCycleDuration.ToString("F1"));
+            flushCycleDuration = l.Slider(flushCycleDuration, 0.1f, 20f);
+
+            l.End();
+
+            Settings_Init.ApplySettingsToDefs();
+
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class Settings_Init
+    {
+        static Settings_Init()
+        {
+            ApplySettingsToDefs();
+        }
+
+        public static void ApplySettingsToDefs()
+        {
+            CompProperties_BiosculpterPod_BaseCycle props = DefDatabase<ThingDef>.GetNamed("BiosculpterPod").CompDefFor<CompBiosculpterPod_FlushCycle>() as CompProperties_BiosculpterPod_BaseCycle;
+            props.durationDays = FlushBiotuning_Settings.flushCycleDuration;
+            //props.durationDays = 0.05f;
+        }
+    }
+
+}
